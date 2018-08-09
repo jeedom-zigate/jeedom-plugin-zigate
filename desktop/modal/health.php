3a60b00bd530ff6c6ab7286f2284025ccc381a61
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2018-08-09 06:45:20 +0200

    Update health.php
    correction bug d'affichage rssi

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -49,13 +49,13 @@ foreach ($eqLogics as $eqLogic) {
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('type') . '</span></td>';
 		$rssi_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
 	if ($eqLogic->getConfiguration('rssi') < 50 && $eqLogic->getConfiguration('rssi') != '') {
-		$rssi_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '%</span>';
-	} elseif ($eqLogic->getConfiguration('rssi') == 50 && $eqLogic->getConfiguration('rssi') != '') {
-		$rssi_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '%</span>';
+		$rssi_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
+	} elseif ($eqLogic->getConfiguration('rssi') < 100 && $eqLogic->getConfiguration('rssi') != '') {
+		$rssi_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
 	} elseif ($eqLogic->getConfiguration('rssi') >= 100 && $eqLogic->getConfiguration('rssi') != '') {
-		$rssi_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '%</span>';
+		$rssi_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
 	} else {
-		$rssi_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
+		$rssi_status = '<span class="label label-primary" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
 	}
 	echo '<td>' . $rssi_status . '</td>';
 	$status = '<span class="label label-success" style="font-size : 1em; cursor : default;">{{OK}}</span>';
