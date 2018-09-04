f90c2f25545e345ed7fe00d20ae3f853dc18af49
ioull <vincent.thevenot@gmail.com>
2018-09-04 10:22:18 +0200

    #51: N'affiche pas le logo secteur quand un équipement est sur batterie et n'as pas d'info batterie.

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -70,14 +70,14 @@ $eqLogics = zigate::byType('zigate');
             }
             echo '<td>' . $status . '</td>';
             $battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
-            if ($eqLogic->getStatus('battery') < 20 && $eqLogic->getStatus('battery') != '') {
+            if ($eqLogic->getConfiguration('power_type') == 1) {
+                $battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
+            } elseif ($eqLogic->getStatus('battery') < 20 && $eqLogic->getStatus('battery') != '') {
                 $battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
             } elseif ($eqLogic->getStatus('battery') < 60 && $eqLogic->getStatus('battery') != '') {
                 $battery_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
             } elseif ($eqLogic->getStatus('battery') > 60 && $eqLogic->getStatus('battery') != '') {
                 $battery_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
-            } else {
-                $battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
             }
             echo '<td>' . $battery_status . '</td>';
             echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
