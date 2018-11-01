528e950bf22c046398c8701ea444848f5150b3af
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2018-11-01 09:31:01 +0100

    Add files via upload
    Affiche en grisé les équipements désactivés

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -45,39 +45,43 @@ $eqLogics = zigate::byType('zigate');
     <tbody>
         <?php
         foreach ($eqLogics as $eqLogic) {
+			$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
             echo '<tr><td><img src="' . $eqLogic->getImage() . '" height="55" width="55" /></td><td><a href="' . $eqLogic->getLinkToConfiguration() .
-                '" style="text-decoration: none;">' . $eqLogic->getHumanName(true) . '</a></td>';
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getId() . '</span></td>';
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('type') . '</span></td>';
-                $rssi_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
+                '" style="' . $opacity . '">' . $eqLogic->getHumanName(true) . '</a></td>';
+            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;' . $opacity . '">' . $eqLogic->getId() . '</span></td>';
+            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;' . $opacity . '">' . $eqLogic->getConfiguration('type') . '</span></td>';
+                $rssi_status = '<span class="label label-success" style="font-size : 1em;' . $opacity . '">{{OK}}</span>';
             if ($eqLogic->getConfiguration('rssi') < 50 && $eqLogic->getConfiguration('rssi') != '') {
-                $rssi_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
+                $rssi_status = '<span class="label label-danger" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getConfiguration('rssi') . '</span>';
             } elseif ($eqLogic->getConfiguration('rssi') < 100 && $eqLogic->getConfiguration('rssi') != '') {
-                $rssi_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
+                $rssi_status = '<span class="label label-warning" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getConfiguration('rssi') . '</span>';
             } elseif ($eqLogic->getConfiguration('rssi') >= 100 && $eqLogic->getConfiguration('rssi') != '') {
-                $rssi_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
+                $rssi_status = '<span class="label label-success" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getConfiguration('rssi') . '</span>';
             } else {
-                $rssi_status = '<span class="label label-primary" style="font-size : 1em;">' . $eqLogic->getConfiguration('rssi') . '</span>';
+                $rssi_status = '<span class="label label-primary" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getConfiguration('rssi') . '</span>';
             }
             echo '<td>' . $rssi_status . '</td>';
-            $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">{{OK}}</span>';
+            $status = '<span class="label label-success" style="font-size : 1em; cursor : default;' . $opacity . '">{{OK}}</span>';
             if ($eqLogic->getStatus('state') == 'nok') {
-                $status = '<span class="label label-danger" style="font-size : 1em; cursor : default;">{{NOK}}</span>';
+                $status = '<span class="label label-danger" style="font-size : 1em; cursor : default;' . $opacity . '">{{NOK}}</span>';
+            }
+            if (!$eqLogic->getIsEnable()) {
+                $status = '<span class="label label-danger" style="font-size : 1em; cursor : default;' . $opacity . '">{{DISABLE}}</span>';
             }
             echo '<td>' . $status . '</td>';
-            $battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
+            $battery_status = '<span class="label label-success" style="font-size : 1em;' . $opacity . '">{{OK}}</span>';
             if ($eqLogic->getConfiguration('power_type') == 1) {
-                $battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
+                $battery_status = '<span class="label label-primary" style="font-size : 1em;' . $opacity . '" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
             } elseif ($eqLogic->getStatus('battery') < 20 && $eqLogic->getStatus('battery') != '') {
-                $battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+                $battery_status = '<span class="label label-danger" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getStatus('battery') . '%</span>';
             } elseif ($eqLogic->getStatus('battery') < 60 && $eqLogic->getStatus('battery') != '') {
-                $battery_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+                $battery_status = '<span class="label label-warning" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getStatus('battery') . '%</span>';
             } elseif ($eqLogic->getStatus('battery') >= 60 && $eqLogic->getStatus('battery') != '') {
-                $battery_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+                $battery_status = '<span class="label label-success" style="font-size : 1em;' . $opacity . '">' . $eqLogic->getStatus('battery') . '%</span>';
             }
             echo '<td>' . $battery_status . '</td>';
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
+            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;' . $opacity . '">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
+            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;' . $opacity . '">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
         }
         ?>
     </tbody>
