8f27fa787ec5c2b44fdadc7d8d84908f8fa77020
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2018-11-14 23:26:32 +0100

    Update zigate.js
    Add Terminal button

--- desktop/js/zigate.js
+++ desktop/js/zigate.js
@@ -19,6 +19,11 @@ $('#bt_healthzigate').on('click', function () {
     $('#md_modal').load('index.php?v=d&plugin=zigate&modal=health').dialog('open');
 });
 
+$('#bt_terminalzigate').on('click', function () {
+    $('#md_modal').dialog({title: "{{Terminal Zigate}}"});
+    $('#md_modal').load('index.php?v=d&plugin=zigate&modal=terminal').dialog('open');
+});
+
 $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 /*
  * Fonction pour l'ajout de commande, appellé automatiquement par plugin.zigate
