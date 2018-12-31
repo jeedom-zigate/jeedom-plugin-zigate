97e9f0ab282d1f9e9c8de95fb80647074b3ff5e1
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2018-12-31 10:02:50 +0100

    Fix install using old pip and avoid removing lib

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -9,8 +9,6 @@ echo "Installation des dépendances"
 sudo apt-get update
 echo 10 > ${PROGRESS_FILE}
 sudo apt-get install -y python3
-echo 20 > ${PROGRESS_FILE}
-sudo apt-get remove -y python3-serial
 echo 30 > ${PROGRESS_FILE}
 sudo apt-get install -y python3-pip
 echo 40 > ${PROGRESS_FILE}
@@ -20,7 +18,7 @@ sudo apt-get install -y python3-setuptools
 echo 70 > ${PROGRESS_FILE}
 BASEDIR=$(dirname "$0")
 zigate_version=$(head -1 $BASEDIR/zigate_version.txt)
-sudo python3 -m pip install zigate==$zigate_version.* --upgrade
+sudo python3 -m pip install zigate==$zigate_version
 echo 100 > ${PROGRESS_FILE}
 echo "Installation des dépendances terminé !"
 rm ${PROGRESS_FILE}
