3821c34dd249811a9d8a997c8e32365f5fcf164b
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-01-29 14:12:50 +0100

    Replace broom icon by shower

--- desktop/php/zigate.php
+++ desktop/php/zigate.php
@@ -75,7 +75,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
             </div>
             <div class="cursor eqLogicAction" id="bt_cleanup_devices" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
                 <p class="text-center">
-                    <i class="fa fa-broom" style="font-size : 6em;color:#767676;"></i>
+                    <i class="fa fa-shower" style="font-size : 6em;color:#767676;"></i>
                 </p>
                 <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><p class="text-center">{{Nettoyer}}</p></span>
             </div>
