edd21a081bf67c588591ce1d19b48ce4c7d4582e
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2019-01-30 11:15:21 +0100

    Update zigate.ajax.php

--- core/ajax/zigate.ajax.php
+++ core/ajax/zigate.ajax.php
@@ -24,14 +24,11 @@
 try {
     require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
     include_file('core', 'authentification', 'php');
-
     if (!isConnect('admin')) {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
-
     ajax::init();
     $action = init('action');
-
     if ($action == 'syncEqLogicWithZiGate') {
         // Admin page: action on click on "Synchroniser". Sync all equipement.
         zigate::syncEqLogicWithZiGate();
@@ -40,21 +37,8 @@ try {
         // eqLogic page: refresh the eqLogic.
         $id = intval(init('args')[0]);
         $eqLogic = zigate::byId($id);
-        $addr = $eqLogic->getConfiguration('addr');
+        $addr = $eqLogic->getLogicalId();
         $result = zigate::callZiGate('refresh_device', [$addr]);
-
-        if ($result['success']) {
-            ajax::success();
-        } else {
-            ajax::error('Echec');
-        }
-    } elseif ($action == 'discover_eqlogic') {
-        // eqLogic page: discover the eqLogic.
-        $id = intval(init('args')[0]);
-        $eqLogic = zigate::byId($id);
-        $addr = $eqLogic->getConfiguration('addr');
-        $result = zigate::callZiGate('discover_device', [$addr]);
-        
         if ($result['success']) {
             ajax::success();
         } else {
@@ -64,17 +48,19 @@ try {
         // eqLogic page: identify the eqLogic.
         $id = intval(init('args')[0]);
         $eqLogic = zigate::byId($id);
-        $addr = $eqLogic->getConfiguration('addr');
+        $addr = $eqLogic->getLogicalId();
         $result = zigate::callZiGate('identify_device', [$addr,10]);
-
         if ($result['success']) {
             ajax::success();
         } else {
             ajax::error('Echec');
         }
-    } elseif ($action == 'send_data') {	
+    } elseif ($action == 'get_version') {
+        $result = zigate::callZiGate($action);
+        ajax::success($result);
+    } elseif ($action == 'send_data') {
         $args = json_decode(init('args'), true);
-        $result = zigate::callZiGate($action,[$args['zigate_command'],$args['zigate_data']]); 
+        $result = zigate::callZiGate($action,[$args['zigate_command'],$args['zigate_data']]);
         ajax::success($result);
     } else {
         // Call method callZiGate with args.
@@ -85,7 +71,6 @@ try {
             ajax::error('Echec');
         }
     }
-
     throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
 } catch (Exception $e) {
     ajax::error(displayExeption($e), $e->getCode());
