a3723ec85c99f8207f1727db677c347d4020f479
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-01-30 09:53:57 +0100

    Add initial empty test

---
+++ tests/zigateTest.php
@@ -0,0 +1,343 @@
+<?php
+
+/**
+ * zigate test case.
+ */
+class zigateTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var zigate
+     */
+    private $zigate;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated zigateTest::setUp()
+
+        $this->zigate = new zigate(/* parameters */);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated zigateTest::tearDown()
+        $this->zigate = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+
+    /**
+     * Tests zigate::callZiGate()
+     */
+    public function testCallZiGate()
+    {
+        // TODO Auto-generated zigateTest::testCallZiGate()
+        $this->markTestIncomplete("callZiGate test not implemented");
+
+        zigate::callZiGate(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::syncEqLogicWithZiGate()
+     */
+    public function testSyncEqLogicWithZiGate()
+    {
+        // TODO Auto-generated zigateTest::testSyncEqLogicWithZiGate()
+        $this->markTestIncomplete("syncEqLogicWithZiGate test not implemented");
+
+        zigate::syncEqLogicWithZiGate(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::syncDevice()
+     */
+    public function testSyncDevice()
+    {
+        // TODO Auto-generated zigateTest::testSyncDevice()
+        $this->markTestIncomplete("syncDevice test not implemented");
+
+        zigate::syncDevice(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::removeDevice()
+     */
+    public function testRemoveDevice()
+    {
+        // TODO Auto-generated zigateTest::testRemoveDevice()
+        $this->markTestIncomplete("removeDevice test not implemented");
+
+        zigate::removeDevice(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->getProperty()
+     */
+    public function testGetProperty()
+    {
+        // TODO Auto-generated zigateTest->testGetProperty()
+        $this->markTestIncomplete("getProperty test not implemented");
+
+        $this->zigate->getProperty(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->evaluateBattery()
+     */
+    public function testEvaluateBattery()
+    {
+        // TODO Auto-generated zigateTest->testEvaluateBattery()
+        $this->markTestIncomplete("evaluateBattery test not implemented");
+
+        $this->zigate->evaluateBattery(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->createCommands()
+     */
+    public function testCreateCommands()
+    {
+        // TODO Auto-generated zigateTest->testCreateCommands()
+        $this->markTestIncomplete("createCommands test not implemented");
+
+        $this->zigate->createCommands(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->update_command()
+     */
+    public function testUpdate_command()
+    {
+        // TODO Auto-generated zigateTest->testUpdate_command()
+        $this->markTestIncomplete("update_command test not implemented");
+
+        $this->zigate->update_command(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->_create_command()
+     */
+    public function test_create_command()
+    {
+        // TODO Auto-generated zigateTest->test_create_command()
+        $this->markTestIncomplete("_create_command test not implemented");
+
+        $this->zigate->_create_command(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->_create_action()
+     */
+    public function test_create_action()
+    {
+        // TODO Auto-generated zigateTest->test_create_action()
+        $this->markTestIncomplete("_create_action test not implemented");
+
+        $this->zigate->_create_action(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::permitJoin()
+     */
+    public function testPermitJoin()
+    {
+        // TODO Auto-generated zigateTest::testPermitJoin()
+        $this->markTestIncomplete("permitJoin test not implemented");
+
+        zigate::permitJoin(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::reset()
+     */
+    public function testReset()
+    {
+        // TODO Auto-generated zigateTest::testReset()
+        $this->markTestIncomplete("reset test not implemented");
+
+        zigate::reset(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::dependancy_info()
+     */
+    public function testDependancy_info()
+    {
+        // TODO Auto-generated zigateTest::testDependancy_info()
+        $this->markTestIncomplete("dependancy_info test not implemented");
+
+        zigate::dependancy_info(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::dependancy_install()
+     */
+    public function testDependancy_install()
+    {
+        // TODO Auto-generated zigateTest::testDependancy_install()
+        $this->markTestIncomplete("dependancy_install test not implemented");
+
+        zigate::dependancy_install(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::deamon_info()
+     */
+    public function testDeamon_info()
+    {
+        // TODO Auto-generated zigateTest::testDeamon_info()
+        $this->markTestIncomplete("deamon_info test not implemented");
+
+        zigate::deamon_info(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::deamon_start()
+     */
+    public function testDeamon_start()
+    {
+        // TODO Auto-generated zigateTest::testDeamon_start()
+        $this->markTestIncomplete("deamon_start test not implemented");
+
+        zigate::deamon_start(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::deamon_stop()
+     */
+    public function testDeamon_stop()
+    {
+        // TODO Auto-generated zigateTest::testDeamon_stop()
+        $this->markTestIncomplete("deamon_stop test not implemented");
+
+        zigate::deamon_stop(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->getImage()
+     */
+    public function testGetImage()
+    {
+        // TODO Auto-generated zigateTest->testGetImage()
+        $this->markTestIncomplete("getImage test not implemented");
+
+        $this->zigate->getImage(/* parameters */);
+    }
+
+    /**
+     * Tests zigate::getVersion()
+     */
+    public function testGetVersion()
+    {
+        // TODO Auto-generated zigateTest::testGetVersion()
+        $this->markTestIncomplete("getVersion test not implemented");
+
+        zigate::getVersion(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->preInsert()
+     */
+    public function testPreInsert()
+    {
+        // TODO Auto-generated zigateTest->testPreInsert()
+        $this->markTestIncomplete("preInsert test not implemented");
+
+        $this->zigate->preInsert(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->postInsert()
+     */
+    public function testPostInsert()
+    {
+        // TODO Auto-generated zigateTest->testPostInsert()
+        $this->markTestIncomplete("postInsert test not implemented");
+
+        $this->zigate->postInsert(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->preSave()
+     */
+    public function testPreSave()
+    {
+        // TODO Auto-generated zigateTest->testPreSave()
+        $this->markTestIncomplete("preSave test not implemented");
+
+        $this->zigate->preSave(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->postSave()
+     */
+    public function testPostSave()
+    {
+        // TODO Auto-generated zigateTest->testPostSave()
+        $this->markTestIncomplete("postSave test not implemented");
+
+        $this->zigate->postSave(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->preUpdate()
+     */
+    public function testPreUpdate()
+    {
+        // TODO Auto-generated zigateTest->testPreUpdate()
+        $this->markTestIncomplete("preUpdate test not implemented");
+
+        $this->zigate->preUpdate(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->postUpdate()
+     */
+    public function testPostUpdate()
+    {
+        // TODO Auto-generated zigateTest->testPostUpdate()
+        $this->markTestIncomplete("postUpdate test not implemented");
+
+        $this->zigate->postUpdate(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->preRemove()
+     */
+    public function testPreRemove()
+    {
+        // TODO Auto-generated zigateTest->testPreRemove()
+        $this->markTestIncomplete("preRemove test not implemented");
+
+        $this->zigate->preRemove(/* parameters */);
+    }
+
+    /**
+     * Tests zigate->postRemove()
+     */
+    public function testPostRemove()
+    {
+        // TODO Auto-generated zigateTest->testPostRemove()
+        $this->markTestIncomplete("postRemove test not implemented");
+
+        $this->zigate->postRemove(/* parameters */);
+    }
+}
+
