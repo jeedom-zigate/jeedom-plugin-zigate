9011e7f2083e48f99d214fa2475a7e2f02b9e11c
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2019-02-02 10:11:22 +0100

    Add files via upload
    Développement du modal réseau Zigate (en cours de dev)

---
+++ desktop/modal/network.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Copyright (c) 2018 Jeedom-ZiGate contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+if (!isConnect('admin')) {
+    throw new Exception('401 - Accès non autorisé');
+}
+$eqLogics = eqLogic::byType('Zigate');
+
+$startTime = config::byKey('lastDeamonLaunchTime', 'Zigate', '{{Jamais lancé}}');
+$awakedDelay = '';
+$versionFirmware = zigate::callZiGate('get_version_text');
+$versionLib = zigate::callZiGate('get_libversion');
+$usbPort = config::byKey('Port', 'Zigate');
+$nodesCount = count($eqLogics);
+?>
+<div id='div_networkZigateAlert' style="display: none;"></div>
+<div class='network' nid='' id="div_templateNetwork">
+    <div class="container-fluid">
+        <div id="content">
+            <ul id="tabs_network" class="nav nav-tabs" data-tabs="tabs">
+                <li class="active"><a href="#summary_network" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Résumé}}</a></li>
+                <li><a href="#actions_network" data-toggle="tab"><i class="fa fa-sliders"></i> {{Actions}}</a></li>
+            </ul>
+            <div id="network-tab-content" class="tab-content">
+                <div class="tab-pane active" id="summary_network">
+                    <br>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading"><h4 class="panel-title">{{Informations}}</h4></div>
+                        <div class="panel-body">
+                            <p>{{Réseau démarré le}} <span class="zigateNetworkAttr label label-default" style="font-size : 1em;" data-l1key="startTime"><?php echo $startTime ?></span></p>
+                            <p>{{Le réseau contient}} <b><span class="zigateNetworkAttr" data-l1key="nodesCount"><?php echo $nodesCount ?></span></b> {{noeuds, actuellement}} </p>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading"><h4 class="panel-title">{{Système}}</h4></div>
+                        <div class="panel-body">
+                            <p>{{Chemin du contrôleur Zigate :}} <span class="zwaveNetworkAttr label label-default" data-l1key="devicePath" style="font-size : 1em;"><?php echo $usbPort ?></span></p>
+                            <p>{{Version du firmware Zigate :}} <span class="zwaveNetworkAttr label label-default" data-l1key="ZigateFirmwareVersion" style="font-size : 1em;"><?php echo $versionFirmware['result']; ?></span></p>
+                            <p>{{Version de la librairie Zigate :}} <span class="zwaveNetworkAttr label label-default" data-l1key="ZigateLibraryVersion" style="font-size : 1em;"><?php echo $versionLib['result']; ?></span></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="actions_network">
+                    <table class="table">
+                        <tr>
+                            <td><a class="btn btn-success bt_addDevice" data-secure="0"><i class="fa fa-refresh"></i> {{Synchroniser}}</a></td>
+                            <td>{{Synchronise les informations du plugin avec les informations réelles du réseau ZigBee.}}</td>
+                        </tr>
+                        <tr>
+                            <td><a class="btn btn-success bt_addDevice" data-secure="0"><i class="fa fa-shower"></i> {{Nettoyer}}</a></td>
+                            <td>{{Vide pour le moment}}</td>
+                        </tr>
+                        <tr>
+                            <td><a class="btn btn-success bt_addDevice" data-secure="0"><i class="fa fa-recycle"></i> {{Reset}}</a></td>
+                            <td>{{équivalent à un débranchement/rebranchement de la ZiGate. Cette commande ne supprime aucune donnée.}}</td>
+                        </tr>
+                        <tr>
+                            <td><a class="btn btn-success bt_addDevice" data-secure="0"><i class="fa fa-eraser"></i> {{Erase PDM}}</a></td>
+                            <td>{{Vide pour le moment}}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
