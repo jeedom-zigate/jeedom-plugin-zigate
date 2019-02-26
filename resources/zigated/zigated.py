fe9abbb0a6d2f5d0821ba2af28d879af9c1c3604
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-02-26 11:08:29 +0100

    don't share http.error

--- resources/zigated/zigated.py
+++ resources/zigated/zigated.py
@@ -202,7 +202,7 @@ def sharedata():
             try_sharedata(key, persistent_file)
             try_sharedata('z'+key, os.path.join(BASE_PATH, 'log', 'zigate'))
             try_sharedata('zu'+key, os.path.join(BASE_PATH, 'log', 'zigate_update'))
-            try_sharedata('http.error.'+key, os.path.join(BASE_PATH, 'log', 'http.error'))
+#             try_sharedata('http.error.'+key, os.path.join(BASE_PATH, 'log', 'http.error'))
         except Exception:
             pass
         time.sleep(60*60*6)
