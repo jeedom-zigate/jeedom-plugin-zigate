f0aaa9d01a788abaa37b60c471089dca0ae62d33
Deepcore93 <42214914+Deepcore93@users.noreply.github.com>
2019-03-07 09:46:04 +0100

    Update zigate.php

--- desktop/php/zigate.php
+++ desktop/php/zigate.php
@@ -83,13 +83,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <center>
                     <i class="fa fa-recycle" style="font-size : 6em;color:#767676;"></i>
                 </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Redémarrage}}</center></span>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Redémarrage Zigate}}</center></span>
             </div>
             <div class="cursor eqLogicAction" id="bt_erasepdm" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 140px;margin-left : 10px;">
                 <center>
                     <i class="fa fa-eraser" style="font-size : 6em;color:#767676;"></i>
                 </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><p class="text-center">{{Remise à zéro}}</p></span>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><p class="text-center">{{Effacement Zigate}}</p></span>
             </div>
             <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 140px;margin-left : 10px;">
                 <center>
