ca49dbe017f0b80cbd9a3e75862389430d16563a
doudz <sebastien.ramage@gmail.com>
2019-11-17 15:19:25 +0100

    Fix text

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -25,5 +25,5 @@ zigate_version=$(head -1 $BASEDIR/zigate_version.txt)
 sudo python3 -m pip install zigate==$zigate_version
 echo 100 > ${PROGRESS_FILE}
 sudo usermod -aG gpio www-data
-echo "Installation des dépendances terminé !"
+echo "Installation des dépendances terminée !"
 rm ${PROGRESS_FILE}
