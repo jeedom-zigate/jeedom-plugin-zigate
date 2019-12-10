23a81d40ab6f9d7c66d376f751df1d260197d25d
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-12-10 17:13:46 +0100

    fix proxy

--- resources/zigated/zigated.py
+++ resources/zigated/zigated.py
@@ -346,7 +346,7 @@ if args.sharedata:
     t.setDaemon(True)
     t.start()
 
-z.start_adminpanel(prefix='plugins/zigate/core/php/zigateproxy.php?url=')
+z.start_adminpanel(prefix='/plugins/zigate/core/php/zigateproxy.php?url=')
 
 t = threading.Thread(target=server.serve_forever)
 t.start()
