5edbea38768477464fe23adf158475b6b4ce9311
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-12-11 15:24:11 +0100

    fix proxy

--- core/php/zigateproxy.php
+++ core/php/zigateproxy.php
@@ -28,7 +28,7 @@
     $headers = getallheaders();
     $headers_str = [];
     #$url = $_GET['url'];
-    $url = 'http://localhost/plugins/zigate/core/php/zigateproxy.php?url='.$_GET['url'];
+    $url = 'http://localhost/'.$_GET['url'];
     
     foreach ( $headers as $key => $value){
       if($key == 'Host')
